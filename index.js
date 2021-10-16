4a7af287d429dcf26e4a80daf0960ee7f60c2201
Luis <louiscrack50@gmail.com>
2021-10-15 19:57:45 -0600

    Montando el servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola, funcionando');
+const express = require('express');
+
+const { createServer } = require('http');
+const app = express();
+const server = createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function(){
+    console.log('La app ha sido arrancada en el puerto '+PORT);
+    
+}) 
