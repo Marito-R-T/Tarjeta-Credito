252b5be005b0c8e38045a2a7b85bc67abaa5a5f5
LouisGonzalez <47205407+LouisGonzalez@users.noreply.github.com>
2021-10-22 01:19:52 -0600

    Delete ApiRouterUnitario.js

--- ApiRouterUnitario.js
+++
@@ -1,4 +0,0 @@
-
-var apiTarjetaRouter=require('./api/TarjetaRouter');
-//si una ruta viene con /tarjeta el encargado de la ruta será apiTarjetaRouter, y se concatena a router
-router.use('/tarjeta', apiTarjetaRouter);
