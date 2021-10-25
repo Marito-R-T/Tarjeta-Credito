73d1b40d0626952c911a1e30ec9bd3db4eb222fb
Marito-R-T <47235291+Marito-R-T@users.noreply.github.com>
2021-10-24 20:43:01 -0600

    pruebas

--- src/Routes/api/PortalPagosRouter.js
+++ src/Routes/api/PortalPagosRouter.js
@@ -1,9 +1,9 @@
 const { Router } = require('express'); //Que usaremos la funcion Router, del paquete express
 const router = Router(); //Creamos un router
-const { vincular_cuenta_bancaria_portal_de_pagos, solicitar_depositar_saldo, solicitar_retirar_saldo } = require('../../Controller/ControladorPortalPago');
+const { vincular_tarjeta_credito_portal_de_pagos, solicitar_depositar_saldo, solicitar_retirar_saldo } = require('../../Controller/ControladorPortalPago');
 
 //rutas para el portal de pagos
-router.post('/tarjeta/verificacion/portal_pagos', vincular_cuenta_bancaria_portal_de_pagos);
+router.post('/tarjeta/verificacion/portal_pagos', vincular_tarjeta_credito_portal_de_pagos);
 router.post('/tarjeta/deposito/portal_pagos', solicitar_depositar_saldo);
 router.post('/tarjeta/retiro/portal_pagos', solicitar_retirar_saldo);
 
