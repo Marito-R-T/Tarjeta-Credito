41950fe7d9928efc9a3efbe99296bb4ab2f880b7
Marito-R-T <47235291+Marito-R-T@users.noreply.github.com>
2021-10-24 21:04:23 -0600

    pruebas2

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
-        username: "arsfiyvykkrhhp",
-        password: "d288a06e72e603119a63b4e1b83690695037c6038fd7b0a0219a44ac07cb25aa",
-        database: "d4baalvuddd4du",
-        host: "ec2-52-0-234-93.compute-1.amazonaws.com",
+        username: "yccotmuvvafkzw",
+        password: "25d69c0382b2e5809a6427a654b10045d5fbae990a44127109ae53db1b29fd0d",
+        database: "d7nkf9no1hm0q0",
+        host: "ec2-44-196-96-149.compute-1.amazonaws.com",
         port: 5432
     }
 }
